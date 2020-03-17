41ea0b8873473589a5320f213ce38ff56edaa811
YuanshuoZhang <yuanshuozhangde@gmail.com>
2020-03-17 18:33:05 +0800

    test

---
+++ Java/Java基础/01变量与数据类型/数据类型/src/DataType.java
@@ -0,0 +1,33 @@
+/*
+    数据类型：Java是一种强类型语言，针对每一种数据都给出了明确的数据类型。
+
+    数据类型的分类：
+        基本数据类型
+        引用数据类型（类、接口、数组）
+
+    基本数据类型：4类8种
+        整数              占用字节数
+            byte          1
+            short         2
+            int           4
+            long          8
+        浮点数
+            float         4
+            double        8
+        字符
+            char          2
+        布尔
+            boolean       1
+    注意：
+        整数默认是int类型，浮点数默认是double类型
+        定义long类型数据的时候，要加L或者l，建议加L
+        定义float类型数据的时候，要加F或者f，建议加F
+
+ */
+
+public class DataType {
+    public static void main(String[] args){
+
+    }
+
+}
