4920dc21b9f3caabb12e1966a7093f50ae21bfc1
YuanshuoZhang <yuanshuozhangde@gmail.com>
2020-04-11 10:01:13 +0800

    update

---
+++ Java/Java基础/packagee/src/com/packagee/PackageDemo.java
@@ -0,0 +1,19 @@
+package com.packagee;
+
+/**
+ * @Author: Yuanshuo
+ * @Date: 2020/4/11 9:22 上午
+ * @Description:
+ *          包的特点：
+ *              可以有多层
+ *              不同包下的文件名可以重复
+ *              包的声明必须是第一行代码
+ *
+ *          不同包之间的相互访问
+ *              类的全名：包名+类名
+ *              使用关键字import将类导入
+ *              import java.util.*
+ */
+public class PackageDemo {
+
+}
