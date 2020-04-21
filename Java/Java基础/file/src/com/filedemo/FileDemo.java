94209756d69f002693db009cb75b302047f0392a
YuanshuoZhang <yuanshuozhangde@gmail.com>
2020-04-21 19:39:34 +0800

    update

--- Java/Java基础/file/src/com/filedemo/FileDemo.java
+++ Java/Java基础/file/src/com/filedemo/FileDemo.java
@@ -4,6 +4,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * @Author: Yuanshuo
@@ -69,8 +70,37 @@ public class FileDemo {
         //boolean isAbsolute(): 判断File对象指向的路径是否是绝对路径，如果是绝对路径则返回true，否则返回false
         ff.createNewFile();
         System.out.println(f.isAbsolute());
-        //boolean isDirectory()
-        //boolean isFile()
-        //boolean isHiddn()
+        //boolean isDirectory(): 判断File对象指向的路径是否是文件夹，是则返回true，否则返回false
+        System.out.println(f2.isDirectory());
+        //boolean isFile(): 判断File对象指向的路径是否是文件，是则返回true，否则返回false
+        System.out.println(ff.isFile());
+        //boolean isHiddn(): 判断File对象指向的路径是否隐藏，是则返回true，否则返回false
+        System.out.println(ff.isHidden());
+
+        //获取
+        //File getAbsoluteFile(): 以File对象的形式返回当前File对象所有指向的绝对路径
+        System.out.println(f.getAbsoluteFile());
+        //String getAbsolutePath()： 返回File对象所指向的绝对路径
+        System.out.println(f.getAbsolutePath());
+        //String getName(): 获取文件和文件夹的名称
+        System.out.println(f.getName());
+        //String getParent()：返回文件父路径
+        System.out.println(f.getParent());
+        //File getParentFile()：返回文件父路径
+        System.out.println(f.getParentFile());
+        //String getPath()：返回创建File对象给的路径
+        System.out.println(ff.getPath());
+        System.out.println(f3.getPath());
+        //long lastModified()：以毫秒值的形式返回最后的修改时间
+        System.out.println(f.lastModified());
+        Date d = new Date(f.lastModified());
+        System.out.println(d.toLocaleString());
+        //long length(): 返回文件的字节数
+        System.out.println(f.length());
+
+        //修改
+        //boolean renameTo(File dest)：将当前File对象所指向的路径 修改为 指定File所指向的路径
+        //注意：修改的文件路径不能存在，如果存在则修改失败
+        System.out.println(ff.renameTo(f));
     }
 }
